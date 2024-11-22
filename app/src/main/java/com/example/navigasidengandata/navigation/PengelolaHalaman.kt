da2f1fd7a496a3c056b230b35aafb851930f803d
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-11-22 11:09:08 +0700

    menambahkan navigasi antar halaman di pengelola halaman, termasuk splash screen, form mahasiswa, dan tampilan rencana studi. juga menghubungkan viewmodel untuk mengelola data pada masing-masing halaman.

--- app/src/main/java/com/example/navigasidengandata/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/navigasidengandata/navigation/PengelolaHalaman.kt
@@ -1,1 +1,18 @@
 package com.example.navigasidengandata.navigation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasidengandata.ui.view.screen.MahasiswaFormView
+import com.example.navigasidengandata.ui.view.screen.RencanaStudyView
+import com.example.navigasidengandata.ui.view.screen.SplashView
+import com.example.navigasidengandata.ui.view.screen.TampilView
+import com.example.navigasidengandata.ui.view.viewmodel.MahasiswaViewModel
+import com.example.navigasidengandata.ui.view.viewmodel.RencanaStudyViewModel
