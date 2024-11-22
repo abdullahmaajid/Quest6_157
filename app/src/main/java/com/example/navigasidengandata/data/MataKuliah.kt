30079bea39dae6c4c58683d7e2d9991ba77da314
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-11-22 11:04:17 +0700

    menambahkan objek MataKuliah dengan variabel nama mata kuliahnya

--- app/src/main/java/com/example/navigasidengandata/data/MataKuliah.kt
+++ app/src/main/java/com/example/navigasidengandata/data/MataKuliah.kt
@@ -1,2 +1,12 @@
 package com.example.navigasidengandata.data
 
+object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pemrograman Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasaran Buatan",
+        "Computer Vision"
+    )
+}
