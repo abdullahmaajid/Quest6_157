91bbb54fe8bb8a412dcc0031a1d12db12bbacf45
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-11-22 11:43:58 +0700

    menambahkan fungsi untuk menyimpan data mahasiswa ke dalam state menggunakan MutableStateFlow. fungsi ini akan memperbarui nilai nim, nama, dan email mahasiswa berdasarkan input dari list.

--- app/src/main/java/com/example/navigasidengandata/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/navigasidengandata/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -6,3 +6,23 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel : ViewModel() {
+
+
+    private val _mahasiswaStateUI = MutableStateFlow(Mahasiswa())
+
+
+    val mahasiswaUiState: StateFlow<Mahasiswa> = _mahasiswaStateUI.asStateFlow()
+
+
+    fun saveDataMahasiswa(ls: MutableList<String>){
+        _mahasiswaStateUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nim = ls[0],
+                nama = ls[1],
+                email = ls[2]
+            )
+        }
+    }
+}
